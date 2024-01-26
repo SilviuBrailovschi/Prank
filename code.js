2b21a9dd72fd5bb62afd3a4c245179dde48417ca
SilviuBrailovschi <121108827+SilviuBrailovschi@users.noreply.github.com>
2024-01-26 11:08:45 +0200

    Add files via upload

---
+++ code.js
@@ -0,0 +1,22 @@
+document.addEventListener("DOMContentLoaded", function () {
+  if (Notification.permission !== "granted") Notification.requestPermission();
+});
+
+function notifyMe() {
+  if (!Notification) {
+    alert("Desktop notifications not available in your browser. Try Chromium.");
+    return;
+  }
+
+  if (Notification.permission !== "granted") Notification.requestPermission();
+  else {
+    var notification = new Notification("Wtf sunshine !", {
+      icon: "./public/images/money-emoji.png",
+      body: "Вы были взломаны!",
+    });
+
+    notification.onclick = function () {
+      window.open("https://pranx.com/hacker/");
+    };
+  }
+}
